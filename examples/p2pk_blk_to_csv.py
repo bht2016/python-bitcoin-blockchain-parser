ccf6e3ea4fd2a5c7de2e185dec7b9519d4a32d2c
boht <boht2023@outlook.com>
2024-11-16 16:34:45 +0800

    update

---
+++ examples/p2pk_blk_to_csv.py
@@ -0,0 +1,180 @@
+import csv
+import json
+import os
+import re
+from collections import namedtuple
+
+from blockchain_parser.address import Address
+from blockchain_parser.block import Block
+from blockchain_parser.blockchain import get_blocks
+
+def save_to_csv(file_path, data_list, fields, overwrite=False):
+    if overwrite:
+        with open(file_path, 'w') as f:
+            writer = csv.writer(f)
+            writer.writerow(fields)
+
+    with open(file_path, 'a') as f:
+        writer = csv.writer(f)
+        writer.writerows(data_list)
+
+
+def save_utxo(file_path, utxo):
+    with open(file_path, 'w') as f:
+        json.dump(utxo, f)
+
+
+def load_utxo(file_path):
+    if os.path.exists(file_path):
+        with open(file_path, 'r') as f:
+            return json.load(f)
+    return {}
+
+def save_checkpoint(file_path, file_id, block_id, trans_id, output_id):
+    with open(file_path, 'w') as f:
+        json.dump({
+            'file_id': file_id,
+            'block_id': block_id,
+            'trans_id': trans_id,
+            'output_id': output_id
+        }, f)
+
+def load_checkpoint(file_path):
+    if os.path.exists(file_path):
+        with open(file_path, 'r') as f:
+            json_data = json.load(f)
+            return json_data['file_id'], json_data['block_id'], json_data['trans_id'], json_data['output_id']
+    return 0, 0, 0, 0
+
+
+Block_fields = ['block_id', 'block_hash', 'version', 'timestamp', 'datetime', 'nonce', 'difficulty', 'merkle_root', 'trans_cnt']
+def block_data(block_id, block):
+    return [block_id, block.hash, block.header.version, block.header.timestamp, block.header.datetime, block.header.nonce, block.header.difficulty, block.header.merkle_root, block.n_transactions]
+
+Transaction_fields = ['trans_id', 'block_id', 'tx_hash', 'version', 'locktime', 'is_segwit', 'is_coinbase', 'intput_cnt', 'ouput_cnt', 'total_satoshis']
+def tx_data(trans_id, block_id, tx, total_satoshis):
+    return [trans_id, block_id, tx.hash, tx.version, tx.locktime, tx.is_coinbase(), tx.is_segwit, tx.n_inputs, tx.n_outputs, total_satoshis]
+
+
+Input_fields = ['trans_id', 'output_id', 'transaction_hash', 'transaction_index', 'unlock_script', 'witness']
+def input_data(trans_id, output_id, input):
+    return [trans_id, output_id, input.transaction_hash, input.transaction_index, input.script, input.witnesses]
+
+Output_fields = ['output_id', 'trans_id', 'output_index', 'addr_type', 'addresses', 'satoshis', 'lock_script', "timestamp"]
+def output_data(output_id, trans_id, output_index, block, output):
+    return [output_id, trans_id, output_index, output.type, extract_addr(output.addresses), output.value, output.script, block.header.timestamp]
+
+
+# pattern = r"addr=([a-fA-F0-9]+)"
+def extract_addr(address):
+    if type(address) == list:
+        if len(address) > 0:
+            addr = address[0]
+            if type(addr) == Address:
+                return addr.address
+
+    return 'Unknow'
+    # match = re.search(pattern, script)
+    # if match:
+    #     # 提取匹配的 addr 内容
+    #     return match.group(1)
+    # else:
+    #     return 'Unkonw'
+
+
+if __name__ == '__main__':
+
+    blocks_dir = '/home/boht/work/pyproj/python-bitcoin-blockchain-parser/block'
+
+    blocks_file = 'data/p2pk_blocks.csv'
+    txs_file = 'data/p2pk_txs.csv'
+    inputs_file = 'data/p2pk_inputs.csv'
+    outputs_file = 'data/p2pk_outputs.csv'
+    utxos_file = 'data/p2pk_utxos.csv'
+
+    checkpoint_file = 'data/checkpoint.txt'
+
+    utxo = load_utxo(utxos_file)
+
+    file_id, block_id, trans_id, output_id = load_checkpoint(checkpoint_file)
+    overwrite = file_id == 0
+
+    print('start from file_id %d, block_id %d, output_id %d' % (file_id, block_id, output_id), flush=True)
+
+    while True:
+        blockfile = os.path.join(blocks_dir, 'blk%05d.dat' % file_id)
+        if not os.path.exists(blockfile):
+            print('blockfile not found: %s' % blockfile, flush=True)
+            break
+
+        print('processing blockfile: %s' % blockfile, flush=True)
+
+        block_list = []
+        tx_list = []
+        input_list = []
+        output_list = []
+
+        for raw_block in get_blocks(blockfile):
+            block = Block(raw_block, block_id, blockfile)
+
+            save_block = False
+            for tx in block.transactions:
+                inputs_tmp_list = []
+                save_tx = False
+                for i, input in enumerate(tx.inputs):
+
+                    oid = -1
+                    if input.transaction_hash in utxo and input.transaction_index in utxo[input.transaction_hash]:
+                        save_tx = True
+                        oid = utxo[input.transaction_hash][input.transaction_index][0]
+                        del utxo[input.transaction_hash][input.transaction_index]
+                        if len(utxo[input.transaction_hash]) == 0:
+                            del utxo[input.transaction_hash]
+
+                    # (trans_id, output_id, input)
+                    inputs_tmp_list.append(input_data(trans_id, oid, input))
+
+                total_satoshis = 0
+                output_tmp_list = []
+                for i, output in enumerate(tx.outputs):
+                    # (output_id, trans_id, output_index, block, output):
+                    output_tmp_list.append(output_data(output_id, trans_id, i, block, output))
+                    total_satoshis += output.value
+
+                    if output.is_pubkey():
+                        save_tx = True
+                        index_set = utxo.get(tx.txid, {})
+                        index_set[i] = (output_id, extract_addr(output.addresses))
+                        utxo[tx.txid] = index_set
+
+                    output_id += 1
+
+                if save_tx:
+                    save_block = True
+                    # (trans_id, block_id, tx, total_satoshis)
+                    tx_list.append(tx_data(trans_id, block_id, tx, total_satoshis))
+                    for item in inputs_tmp_list:
+                        input_list.append(item)
+                    for item in output_tmp_list:
+                        output_list.append(item)
+
+                trans_id += 1
+
+            if save_block:
+                # (block_id, block)
+                block_list.append(block_data(block_id, block))
+
+            block_id += 1
+
+        # Save to CSV
+        save_to_csv(blocks_file, block_list, Block_fields, overwrite)
+        save_to_csv(txs_file, tx_list, Transaction_fields, overwrite)
+        save_to_csv(inputs_file, input_list, Input_fields, overwrite)
+        save_to_csv(outputs_file, output_list, Output_fields, overwrite)
+        overwrite = False
+
+        save_utxo(utxos_file, utxo)
+
+        file_id += 1
+        save_checkpoint('data/checkpoint.txt', file_id, block_id, trans_id, output_id)
+        print("Finished blockfile %s, next: file_id %d, block_id %d, output_id %d, utxo: %d" % (blockfile, file_id, block_id, output_id, len(utxo)), flush=True)
---
+++ examples/p2pk_csv_to_mysql.py
@@ -0,0 +1,279 @@
+import csv
+import json
+import os
+import re
+
+import pymysql
+
+
+def load_utxo(file_path):
+    if os.path.exists(file_path):
+        with open(file_path, 'r') as f:
+            return json.load(f)
+    return {}
+
+def read_csv(file_path):
+    with open(file_path, 'r') as f:
+        reader = csv.reader(f)
+        head = True
+        for row in reader:
+            if head:
+                head = False
+                continue
+            yield row
+
+def save_to_mysql(preSQLs, insertSQL, data_list):
+    try:
+        conn = pymysql.connect(host='127.0.0.1', port=3306, user='root', password='123456', db='p2pk', charset="utf8")
+
+        with conn.cursor() as cursor:
+            for sql in preSQLs:
+                cursor.execute(sql)
+            conn.commit()
+
+            batch = []
+            for item in data_list:
+                batch.append(item)
+                if len(batch) > 10000:
+                    cursor.executemany(insertSQL, batch)
+                    conn.commit()
+                    batch = []
+
+            if len(batch) > 0:
+                cursor.executemany(insertSQL, batch)
+                conn.commit()
+
+    except Exception as e:
+        print(e)
+    finally:
+        conn.close()
+
+
+
+def block_to_mysql():
+
+    block_data_list = []
+    for row in read_csv('data/p2pk_blocks.csv'):
+
+        # ['block_id', 'block_hash', 'version', 'timestamp', 'datetime', 'nonce', 'difficulty', 'merkle_root', 'trans_cnt']
+
+        block_id = int(row[0])
+        block_hash = row[1]
+        version = int(row[2])
+        timestamp = int(row[3])
+        datetime = row[4]
+        nonce = int(row[5])
+        difficulty = float(row[6])
+        merkle_root = row[7]
+        trans_cnt = int(row[8])
+        block_data_list.append((block_id, block_hash, version, timestamp, nonce, difficulty, merkle_root, trans_cnt))
+
+    sql = ["""
+        DROP TABLE IF EXISTS `p2pk_blocks`
+        """,
+        """
+        CREATE TABLE `p2pk_blocks` (
+            `block_id` int(11) NOT NULL PRIMARY KEY,
+            `block_hash` varchar(128) NOT NULL,
+            `version` int(11) NOT NULL,
+            `timestamp` int(11) NOT NULL,
+            `nonce` bigint(11) NOT NULL,
+            `difficulty` double NOT NULL,
+            `merkle_root` varchar(128) NOT NULL,
+            `trans_cnt` int(11) NOT NULL
+        ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+        """]
+
+    insertSQL = "INSERT INTO p2pk_blocks(block_id, block_hash, version, timestamp, nonce, difficulty, merkle_root, trans_cnt) VALUES(%s, %s, %s, %s, %s, %s, %s, %s)"
+    save_to_mysql(sql, insertSQL, block_data_list)
+
+
+def transaction_to_mysql():
+
+    tx_data_list = []
+    for row in read_csv('data/p2pk_txs.csv'):
+
+        # trans_id,block_id,tx_hash,version,locktime,is_segwit,is_coinbase,intput_cnt,ouput_cnt,total_satoshis
+
+        tx_id = int(row[0])
+        block_id = int(row[1])
+        tx_hash = row[2]
+        version = int(row[3])
+        locktime = int(row[4])
+        is_segwit = 1 if row[5] == 'True' else 0
+        is_coinbase = 1 if row[6] == 'True' else 0
+        input_cnt = int(row[7])
+        ouput_cnt = int(row[8])
+        total_satoshis = int(row[9])
+
+        tx_data_list.append((tx_id, block_id, tx_hash, version, locktime, is_segwit, is_coinbase, input_cnt, ouput_cnt, total_satoshis))
+
+
+    sql = ["""
+        DROP TABLE IF EXISTS `p2pk_transactions`
+        """,
+        """
+        CREATE TABLE `p2pk_transactions` (
+            `tx_id` int(11) NOT NULL PRIMARY KEY,
+            `block_id` int(11) NOT NULL,
+            `tx_hash` varchar(126) NOT NULL,
+            `version` int(11) NOT NULL,
+            `locktime` int(11) NOT NULL,
+            `is_segwit` bit(1) NOT NULL,
+            `is_coinbase` bit(1) NOT NULL,
+            `input_cnt` int(5)NOT NULL,
+            `ouput_cnt` int(5)NOT NULL,
+            `total_satoshis` bigint(5) NOT NULL
+         ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+        """
+    ]
+
+    insertSQL = "INSERT INTO p2pk_transactions(tx_id, block_id, tx_hash, version, locktime, is_segwit, is_coinbase, input_cnt, ouput_cnt, total_satoshis) VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
+    save_to_mysql(sql, insertSQL, tx_data_list)
+
+
+def input_to_mysql():
+
+    input_data_list = []
+    for row in read_csv('data/p2pk_inputs.csv'):
+        # trans_id,output_id,transaction_hash,transaction_index,unlock_script,witness
+
+        tx_id = int(row[0])
+        output_id = int(row[1])
+        tx_hash = row[2]
+        tx_index = int(row[3])
+        unlock_script = row[4]
+        witness = row[5]
+
+        if tx_index >= 0xFFFFFFFF:
+            tx_index = -1
+
+        input_data_list.append((tx_id, output_id, tx_hash, tx_index, unlock_script, witness))
+
+    sql = ["""
+        DROP TABLE IF EXISTS `p2pk_inputs`
+        """,
+        """
+        CREATE TABLE `p2pk_inputs` (
+            `tx_id` int(11) NOT NULL,
+            `output_id` int(11) NOT NULL,
+            `transaction_hash` varchar(128) NOT NULL,
+            `transaction_index` int(11) NOT NULL,
+            `unlock_script` text NOT NULL,
+            `witness` varchar(1024) NOT NULL
+        ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+        """
+    ]
+
+    insertSQL = "INSERT INTO p2pk_inputs(tx_id, output_id, transaction_hash, transaction_index, unlock_script, witness) VALUES(%s, %s, %s, %s, %s, %s)"
+    save_to_mysql(sql, insertSQL, input_data_list)
+
+
+def output_to_mysql():
+
+    addr_map = {}
+    addr_id = 0
+    utxo = load_utxo('data/p2pk_utxos.csv')
+    for txid, output in utxo.items():
+        for index, (output_id, addr) in output.items():
+            addr_info = addr_map.get(addr, {})
+            addr_info['addr_id'] = addr_id
+            addr_id += 1
+            output_id_set = addr_info.get('output_id_set', set())
+            output_id_set.add(output_id)
+            addr_info['output_id_set'] = output_id_set
+
+            addr_map[addr] = addr_info
+
+
+    output_data_list = []
+    for row in read_csv('data/p2pk_outputs.csv'):
+        # output_id,trans_id,output_index,addr_type,addresses,satoshis,lock_script,timestamp
+
+        output_id = int(row[0])
+        tx_id = int(row[1])
+        output_index = int(row[2])
+        addr_type = row[3]
+        address = row[4]
+        satoshis = int(row[5])
+        lock_script = row[6]
+        timestamp = int(row[7])
+
+        if address == 'Unkonw':
+            continue
+
+        relation_addr = -1
+        if address in addr_map:
+            addr_info = addr_map[address]
+            relation_addr = int(addr_info['addr_id'])
+            addr_info['tx_count'] = addr_info.get('tx_count', 0) + 1
+            addr_info['first_time'] = min(addr_info.get('first_time', 0xFFFFFFFF), timestamp)
+            addr_info['last_time'] = max(addr_info.get('last_time', 0), timestamp)
+
+            if output_id in addr_info['output_id_set']:
+                addr_info['satoshis'] = addr_info.get('satoshis', 0) + int(satoshis)
+
+
+        output_data_list.append((output_id, tx_id, output_index, relation_addr, addr_type, address, satoshis, lock_script, timestamp))
+
+
+    sql = ["""
+        DROP TABLE IF EXISTS `p2pk_outputs`
+        """,
+        """
+        CREATE TABLE `p2pk_outputs` (
+            `output_id` int(11) NOT NULL PRIMARY KEY,
+            `trans_id` int(11) NOT NULL,
+            `output_index` int(11) NOT NULL,
+            `relation_addr` int(11) NOT NULL,
+            `addr_type` varchar(32) NOT NULL,
+            `address` varchar(256) NOT NULL,
+            `satoshis` bigint(11) NOT NULL,
+            `lock_script` text NOT NULL,
+            `timestamp` int(11) NOT NULL
+        ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+        """
+    ]
+
+    insertSQL = "INSERT INTO p2pk_outputs(output_id, trans_id, output_index, relation_addr, addr_type, address, satoshis, lock_script, timestamp) VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s)"
+    save_to_mysql(sql, insertSQL, output_data_list)
+
+    address_to_mysql(addr_map)
+
+
+def address_to_mysql(addr_map):
+
+    addr_data_list = []
+    for addr, addr_info in addr_map.items():
+        addr_data_list.append((addr_info['addr_id'], addr, addr_info['satoshis'], addr_info['tx_count'], addr_info['first_time'], addr_info['last_time']))
+
+    sql = ["""
+        DROP TABLE IF EXISTS `p2pk_address`
+        """,
+        """
+        CREATE TABLE `p2pk_address` (
+            `addr_id` int(11) NOT NULL PRIMARY KEY,
+            `address` varchar(256) NOT NULL,
+            `satoshis` bigint(11) NOT NULL,
+            `tx_count` int(11) NOT NULL,
+            `first_time` int(11) NOT NULL,
+            `last_time` int(11) NOT NULL
+        ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+        """
+    ]
+
+    insertSQL = "INSERT INTO p2pk_address(addr_id, address, satoshis, tx_count, first_time, last_time) VALUES(%s, %s, %s, %s, %s, %s)"
+    save_to_mysql(sql, insertSQL, addr_data_list)
+
+
+if __name__ == '__main__':
+    # block_to_mysql()
+    # transaction_to_mysql()
+    # input_to_mysql()
+    output_to_mysql()
+
+
+
+
+
+
